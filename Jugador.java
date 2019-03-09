df130d1dae7bc4857cd48be68f9811a16f7c0cc4
Pablo Oraá <35403993+PabloOraa@users.noreply.github.com>
2019-03-09 21:20:58 +0100

    Corrección en el Javadoc
    Cambios en los comentarios que conforman el Javadoc de la aplicación para añadir la versión, correcciones gramaticales y otros cambios menores.
    En el caso de las Excepciones se han añadido los comentarios pertinentes

---
+++ Jugador.java
@@ -0,0 +1,63 @@
+package Clases;
+
+import java.util.ArrayList;
+
+/**
+ * Clase Jugador que representa a cada uno de los miembros de la partida
+ * @Version 1.0
+ * @author Enrique Dominguez, David Mateos, Pablo Oraa
+ */
+public class Jugador
+{
+    private final String nickname;
+    private Tablero tableroBarcos;
+    private Tablero tabletoResultados;
+    private ArrayList<Barco> listaBarcos;
+
+    public Jugador(String nickname)
+    {
+        this.nickname = nickname;
+        tableroBarcos = new Tablero();
+        tabletoResultados = new Tablero();
+        listaBarcos = new ArrayList<>();
+        crearListaBarcos();
+    }
+    
+    private void crearListaBarcos()
+    {
+        listaBarcos.add(new Barco("portaaviones",5));
+        listaBarcos.add(new Barco("buque",3));
+        listaBarcos.add(new Barco("submarino",2));
+        listaBarcos.add(new Barco("lancha",1));
+    }
+    
+    public String Disparar(Jugador j2, int fila, char columna)
+    {
+        return j2.comprobarDisparo(fila,columna);
+    }
+    
+    public String comprobarDisparo(int fila, char columna)
+    {
+        char res;
+        String resultado;
+        if(Textos.EMPTY != tableroBarcos.getPos(fila,columna))
+        {
+            res = Textos.RIGHTLETTER;
+            resultado = Textos.RIGHT;
+        }
+        else
+        {
+            res = Textos.FAILLETTER;
+            resultado = Textos.FAIL;
+        }
+        referenciaTablero(res);
+        return resultado;
+    }
+
+    private void referenciaTablero(char res)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
